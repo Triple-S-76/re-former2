38139f7f646a0961a7dc2ea46684385bee89b1d2
TripleS <triple-s-76@outlook.com>
2024-06-19 04:46:54 -0700

    finished HTML form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,28 @@
 class UsersController < ApplicationController
 
   def new
+    p 'USERS NEW JUST RAN'
   end
 
   def create
+    p 'USERS CREATE JUST RAN'
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
+    p 123
+    p @user
+    p 456
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    p 'USER PARAMS JUST RAN'
+    params.require(:user).permit(:username, :email, :password)
   end
 end
